74653409e58ad8850daee813d63aa03b1d55b9a5
Reka Parathalwar <Reka Parathalwar>
2015-11-21 13:18:19 +0000

    FIrst commit

---
+++ src/main/java/com/calc/engine/CalcEngine.java
@@ -0,0 +1,10 @@
+package com.calc.engine;
+
+/**
+ * Created by Reka on 21-11-2015.
+ */
+public class CalcEngine {
+    public static void main(String[] args) {
+        System.out.println("1+2=3");
+    }
+}
